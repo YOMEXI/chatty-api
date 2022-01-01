af6b17f20afa166df26ca71ea89f93acfbee87eb
YOMEXI <abayomexy@gmail.com>
2022-01-01 20:41:50 +0100

    v3

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -103,7 +103,7 @@ const login = asyncHandler(
       cookie.serialize("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 360000,
         path: "/",
       })
